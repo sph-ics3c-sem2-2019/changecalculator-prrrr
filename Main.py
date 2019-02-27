f7d7d9f99f12637839965a0b9b4f77921b4db18d
TheAddel <32132583+TheAddel@users.noreply.github.com>
2019-02-27 14:38:17 -0500

    first project
    first project

--- Main.py
+++ Main.py
@@ -5,4 +5,13 @@
 # Break the change into how many nickels, dimes, quarters
 # loonies, toonies, $5s, $10s, $20s, $50s, $100s
 # If amount is below the cost, say how much more they owe.
-cost=float(input("How much does the item cost?"))
+Cost=float(input ("How much does the item cost?"))
+amount=float(input("what the payment?"))
+change=amount-cost
+print("Your change is",change)
+num100=change//100
+print(num100,"x $100")
+change=change&100
+num100=change//50
+print(num100,"x $50")
+change=change&50
